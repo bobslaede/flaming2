956657b9ddaf2ba07e558c2ff685da6ba3422919
Jeppe Dyrby <jeppe.dyrby@gmail.com>
2015-06-03 20:04:57 +0000

    gulp stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
 var vulcanize = require('gulp-vulcanize');
+var htmlreplace = require('gulp-html-replace');
 
 gulp.task('build:html', function() {
     return gulp.src('src/index.html')
         .pipe(vulcanize())
+        .pipe(htmlreplace({
+            js: {
+                src: null
+            }
+        }))
         .pipe(gulp.dest('dist'))
 })
