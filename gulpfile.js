be6a237ab44d0b86b16a02247ec891b867365727
Jeppe Dyrby <jeppe.dyrby@gmail.com>
2015-06-03 20:13:00 +0000

    gulp stuff build html

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 var gulp = require('gulp');
 var vulcanize = require('gulp-vulcanize');
-var htmlreplace = require('gulp-html-replace');
+var useref = require('gulp-useref');
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
 
 gulp.task('build:html', function() {
+    
+    var assets = useref.assets();
+    
     return gulp.src('src/index.html')
         .pipe(vulcanize())
-        .pipe(htmlreplace({
-            js: {
-                src: null
-            }
-        }))
+        .pipe(assets)
+      //  .pipe(gulpif('*.js', uglify()))
+        .pipe(assets.restore())
+        .pipe(useref())
         .pipe(gulp.dest('dist'))
 })
