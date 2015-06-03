60ab87a361318903331b5caf2b9b5a5c5efb516c
Jeppe Dyrby <jeppe.dyrby@gmail.com>
2015-06-03 19:36:44 +0000

    cleanup

---
+++ typings/tsd.d.ts
@@ -0,0 +1,6 @@
+
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="browserify/browserify.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="es6-promise/es6-promise.d.ts" />
