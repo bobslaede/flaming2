06c51f169e8a5f3459920a8d955116f78587d0e1
jdyrby <jdyrby@ebay.com>
2015-06-04 09:40:16 +0200

    cool stuff with gulp, less, elements

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,31 @@ var vulcanize = require('gulp-vulcanize');
 var useref = require('gulp-useref');
 var gulpif = require('gulp-if');
 var uglify = require('gulp-uglify');
+var less = require('gulp-less');
 
 gulp.task('build:html', function() {
     
     var assets = useref.assets();
     
     return gulp.src('src/index.html')
-        .pipe(vulcanize())
         .pipe(assets)
       //  .pipe(gulpif('*.js', uglify()))
         .pipe(assets.restore())
         .pipe(useref())
         .pipe(gulp.dest('dist'))
+
+})
+
+gulp.task('build:elements', function () {
+    return gulp.src('src/elements/elements.html')
+        .pipe(vulcanize())
+        .pipe(gulp.dest('dist/elements'))
+})
+
+gulp.task('build:css', function () {
+
+    return gulp.src('src/**/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('dist'))
+
 })
