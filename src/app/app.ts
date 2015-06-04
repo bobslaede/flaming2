e1a2e484be3b8e302d69ceed219738a76b9a98b1
jdyrby <jdyrby@ebay.com>
2015-06-04 14:51:16 +0200

    angular is working

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,15 @@
 
-export class App {
-    constructor() {
-        console.log('foo;', this)
-    }
-}
+import angular from 'angular';
+
+var app = angular.module('app', [])
+    .directive('myApp', function () {
+        return {
+            restrict: 'E',
+            scope: true,
+            template: `<div class="app">
+                <paper-button raised>FooBar</paper-button>
+            </div>`
+        }
+    })
+
+export var app = app;
--- src/init.ts
+++ src/init.ts
@@ -1,17 +1,5 @@
-import angular from 'angular/angular'
+import angular from 'angular'
 
-import {App} from 'app/app'
+import {app} from 'app/app'
 
-export class Foo {
-    foo:string;
-
-    constructor() {
-        this.foo = 'bar';
-    }
-}
-
-var app = new App();
-
-var a = [1, 2].map(x => x * 2);
-
-console.log(angular);
+angular.bootstrap(document.documentElement, [app.name])
