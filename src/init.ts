754f83b33247c47dea28b7ccb9d70d7bf49a073b
Jeppe Dyrby <jeppe.dyrby@gmail.com>
2015-06-04 23:38:06 +0200

    fucked up way to make systemjs load relative urls from within modules

--- src/init.ts
+++ src/init.ts
@@ -1,8 +1,48 @@
 /// <reference path="../typings/tsd.d.ts" />
 
-import * as angular from 'angular'
+import {Component, View, bootstrap} from 'angular2/angular2'
 
-import {app} from 'app/app'
+@Component({
+    selector: 'my-app'
+})
+@View({
+    template: `
+        <paper-drawer-panel>
+          <paper-header-panel drawer>
+            <paper-toolbar>
+              <div>Application</div>
+            </paper-toolbar>
+            <div>
+                <paper-menu>
+                    <paper-item>
+                        foo
+                    </paper-item>
+                </paper-menu>
+            </div>
+          </paper-header-panel>
+          <paper-header-panel main>
+            <paper-toolbar>
+              <paper-icon-button icon="menu" paper-drawer-toggle></paper-icon-button>
+              <div>Title</div>
+            </paper-toolbar>
+            <div> Main content...
 
-angular.bootstrap(document.documentElement, [app.name])
+                <p>
+                    <div class="icons">
+                        eject
+                    </div>
+                </p>
+            </div>
+          </paper-header-panel>
+        </paper-drawer-panel>
+    `
+})
+export class MyApp {
 
+    constructor() {
+        console.log('foo');
+    }
+
+}
+
+bootstrap(MyApp);
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="angularjs/angular.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="es6-promise/es6-promise.d.ts" />
+/// <reference path="angular2/angular2.d.ts" />
